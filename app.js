ef2e343da25434f9e16b7935176cb7965cefb8d7
Samar-Memon <samar56786raza@gmail.com>
2024-05-02 10:51:39 +0500

    Snake-Game

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const controls = document.querySelectorAll('.controls i');
 
 let gameOver = false;
 let foodX, foodY;
-let snakeX = 5, snakeY = 10;
+let snakeX = 28, snakeY = 5;
 let snakeBody = [];
 let velocityX = 0, velocityY = 0;
 let setInterValid,
